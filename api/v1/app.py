f7154bd7e1fecfdab7e73aa3cba6cdada31c520d
jemal1444 <jemal.eyasu@yahoo.com>
2023-10-29 17:01:49 +0300

    This is an ALX project on 0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
-'''
-Createw Flask app; and register the blueprint app_views to Flask instance app.
-'''
+"""Module contains flask app"""
 
-from os import getenv
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+from flask import jsonify
+from flask_cors import CORS
+from flask import make_response
 
 app = Flask(__name__)
-
-# enable CORS and allow for origins:
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
-
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def teardown_engine(exception):
-    '''
-    Removes the current SQLAlchemy Session object after each request.
-    '''
+def closeStorage(self):
+    """Closes the storage session"""
     storage.close()
 
 
-# Error handlers for expected app behavior:
 @app.errorhandler(404)
-def not_found(error):
-    '''
-    Return errmsg `Not Found`.
-    '''
-    response = {'error': 'Not found'}
-    return jsonify(response), 404
+def page_not_found(err):
+    """Custom 404 page not found json error message"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    useHost = getenv('HBNB_API_HOST') or '0.0.0.0'
+    usePort = getenv('HBNB_API_PORT') or 5000
 
+    app.run(host=useHost, port=usePort, threaded=True)
